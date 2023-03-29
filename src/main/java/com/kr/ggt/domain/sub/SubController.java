afd5fe35f8119a0368eac680bf566e03c6a5795a
장현우 <haun_woo@naver.com>
2023-03-29 17:46:00 +0900

    장부페이지완료, 투표페이지구성 진행중

--- src/main/java/com/kr/ggt/domain/sub/SubController.java
+++ src/main/java/com/kr/ggt/domain/sub/SubController.java
@@ -9,13 +9,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class SubController {
 
+    //장부페이지이동
     @GetMapping("/sub/accountBook.do")
     public String accountBook(Model model){
-
-
-
-
         return "sub/acountBook";
+    }
+
+    //투표페이지이동
+    @GetMapping("/sub/vote.do")
+    public String vote(Model model){
+        
+        //로그인 체크 및 투표 여부 확인해야함
+        
+        return "sub/vote";
     }
 
 
